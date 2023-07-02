66a859710ffeb9cbf267b825542d0df2990be174
maotterson <otterson.2@gmail.com>
2023-07-02 01:05:27 -0400

    add maui app to playground

---
+++ MudBlazorPlayground.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MudBlazorPlayground.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MudBlazorPlayground.Mobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MudBlazorPlayground.Mobile;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
