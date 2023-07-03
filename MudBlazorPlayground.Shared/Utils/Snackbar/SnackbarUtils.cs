74c4bc70511dcdeac8f5b6a77a5a9ad4b0228ebd
maotterson <otterson.2@gmail.com>
2023-07-03 01:52:33 -0400

    set snackbar configuration settings

---
+++ MudBlazorPlayground.Shared/Utils/Snackbar/SnackbarUtils.cs
@@ -0,0 +1,17 @@
+﻿using MudBlazor;
+
+namespace MudBlazorPlayground.Shared.Utils.Snackbar;
+public static class SnackbarUtils
+{
+    public static void SetSnackbarConfiguration(this SnackbarConfiguration snackbarConfiguration)
+    {
+        snackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+        snackbarConfiguration.PreventDuplicates = true;
+        snackbarConfiguration.NewestOnTop = false;
+        snackbarConfiguration.ShowCloseIcon = true;
+        snackbarConfiguration.VisibleStateDuration = 10000;
+        snackbarConfiguration.HideTransitionDuration = 500;
+        snackbarConfiguration.ShowTransitionDuration = 500;
+        snackbarConfiguration.SnackbarVariant = Variant.Filled;
+    }
+}
--- MudBlazorPlayground.WASM/Program.cs
+++ MudBlazorPlayground.WASM/Program.cs
@@ -2,12 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using MudBlazorPlayground.WASM;
+using MudBlazorPlayground.Shared.Utils.Snackbar;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.SetSnackbarConfiguration();
+});
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
