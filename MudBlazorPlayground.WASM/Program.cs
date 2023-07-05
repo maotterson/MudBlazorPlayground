77d485ea11a986d399b33cfb0d3233d8cee7f595
maotterson <otterson.2@gmail.com>
2023-07-05 01:41:10 -0400

    add httpclient + service in di container

--- MudBlazorPlayground.WASM/Program.cs
+++ MudBlazorPlayground.WASM/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using MudBlazorPlayground.WASM;
 using MudBlazorPlayground.Shared.Utils.Snackbar;
+using MudBlazorPlayground.WASM.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,5 +14,8 @@ builder.Services.AddMudServices(config =>
     config.SnackbarConfiguration.SetSnackbarConfiguration();
 });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<CountryService>();
 
 await builder.Build().RunAsync();
