355798a248ee6c771e7d0950fda81c5867c325eb
maotterson <otterson.2@gmail.com>
2023-07-05 01:36:33 -0400

    migrate service/model for countries to wasm

---
+++ MudBlazorPlayground.WASM/Domain/CountryFlag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MudBlazorPlayground.WASM.Domain;
+public class CountryFlag
+{
+    [JsonPropertyName("country")]
+    public string Country { get; set; } = default!;
+    [JsonPropertyName("flag_base64")]
+    public string Flag { get; set; } = default!;
+}
---
+++ MudBlazorPlayground.WASM/Services/CountryService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.FileProviders;
+using MudBlazorPlayground.WASM.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MudBlazorPlayground.WASM.Services;
+public class CountryService
+{
+
+    public async Task<List<CountryFlag>> GetCountriesAsync()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        string resourcePath = "MudBlazorPlayground.Mobile.Resources.Raw.country-by-flag.json";
+
+        using (Stream stream = assembly.GetManifestResourceStream(resourcePath)!)
+        {
+            if (stream is null)
+            {
+                throw new FileNotFoundException();
+            }
+
+            return await JsonSerializer.DeserializeAsync<List<CountryFlag>>(stream)!;
+        }
+    }
+}
